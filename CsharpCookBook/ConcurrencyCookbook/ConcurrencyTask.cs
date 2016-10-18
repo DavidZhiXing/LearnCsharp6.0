03f16e96b181404c89d87f95725e42b7e20e1498
DavidZhiXing <wow_only@163.com>
2016-10-18 16:06:09 +0800

    Add Async Collection

--- CsharpCookBook/ConcurrencyCookbook/ConcurrencyTask.cs
+++ CsharpCookBook/ConcurrencyCookbook/ConcurrencyTask.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Nito.AsyncEx;
+using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -7,6 +8,7 @@ using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
 
 namespace ConcurrencyCookbook
 {
@@ -303,6 +305,144 @@ namespace ConcurrencyCookbook
         internal void Rotate(float degrees)
         {
             throw new NotImplementedException();
+        }
+
+        readonly BlockingCollection<int> _blcokingQuene =
+            new BlockingCollection<int>();
+        BlockingCollection<int> _bQuene = new BlockingCollection<int>(boundedCapacity: 1);
+        BlockingCollection<int> _blockingStack = new BlockingCollection<int>(
+            new ConcurrentStack<int>());
+        BlockingCollection<int> _blockingBag = new BlockingCollection<int>(
+            new ConcurrentBag<int>()); 
+
+        
+
+        public Matrix()
+        {
+            _blcokingQuene.Add(7);
+            _blcokingQuene.Add(13);
+            _blcokingQuene.CompleteAdding();
+            foreach (var item in _blcokingQuene.GetConsumingEnumerable())
+            {
+                Trace.WriteLine(item);
+            }
+
+
+            _blockingStack.Add(7);
+            _blockingStack.Add(13);
+            _blockingStack.CompleteAdding();
+            foreach (var item in _blockingStack.GetConsumingEnumerable())
+            {
+                Trace.WriteLine(item);
+            }
+
+
+
+            _blockingBag.Add(7);
+            _blockingBag.Add(13);
+            _blockingBag.CompleteAdding();
+            foreach (var item in _blockingBag.GetConsumingEnumerable())
+            {
+                Trace.WriteLine(item);
+            }
+            var newValue = dic.AddOrUpdate(0,
+                key => "zero",
+                (key, oldvalue) => "zero");
+            dic[0] = "zero";
+            string currentValue;
+            bool keyExists = dic.TryGetValue(0, out currentValue);
+            string removeValue;
+            bool keyExisted = dic.TryRemove(0, out removeValue);
+        }
+
+        BufferBlock<int> _asyncQueue = new BufferBlock<int>();
+        AsyncProducerConsumerQueue<int> _asyncPQueue =
+            new AsyncProducerConsumerQueue<int>();
+        AsyncCollection<int> _asyncStack = new AsyncCollection<int>(
+            new ConcurrentStack<int>());
+        AsyncCollection<int> _asyncBags = new AsyncCollection<int>(
+             new ConcurrentBag<int>());
+        ConcurrentDictionary<int, string> dic = new ConcurrentDictionary<int, string>();
+        ActionBlock<int> quen = new ActionBlock<int>(item => Trace.WriteLine(item));
+        async Task test()
+        {
+            await _asyncQueue.SendAsync(7);
+            await _asyncQueue.SendAsync(13);
+            _asyncQueue.Complete();
+            while (await _asyncQueue.OutputAvailableAsync())
+            {
+                Trace.WriteLine(await _asyncQueue.ReceiveAsync());
+            }
+            while (true)
+            {
+                int item;
+                try
+                {
+                    item = await _asyncQueue.ReceiveAsync();
+                }
+                catch (InvalidOperationException)
+                {
+
+                    break;
+                }
+                Trace.WriteLine(item);
+            }
+
+            _asyncQueue.Post(7);
+            _asyncQueue.Post(13);
+            _asyncQueue.Complete();
+            while (true)
+            {
+                int item;
+                try
+                {
+                    item = await _asyncQueue.ReceiveAsync();
+                }
+                catch (InvalidOperationException)
+                {
+
+                    break;
+                }
+                Trace.WriteLine(item);
+            }
+
+            await quen.SendAsync(7);
+            await quen.SendAsync(13);
+            quen.Post(7);
+            quen.Post(13);
+            quen.Complete();
+
+
+
+            await _asyncPQueue.EnqueueAsync(7);
+            await _asyncPQueue.EnqueueAsync(13);
+            _asyncPQueue.CompleteAdding();
+            while (await _asyncPQueue.OutputAvailableAsync())
+            {
+                Trace.WriteLine(await _asyncPQueue.DequeueAsync());
+            }
+
+            await _asyncStack.AddAsync(7);
+            await _asyncStack.AddAsync(13);
+            _asyncStack.CompleteAdding();
+            while (await _asyncStack.OutputAvailableAsync())
+            {
+                Trace.WriteLine(await _asyncStack.TakeAsync());
+            }
+            _asyncStack = new AsyncCollection<int>(
+                new ConcurrentStack<int>(), maxCount: 1);
+            await _asyncStack.AddAsync(7);
+            await _asyncStack.AddAsync(13);
+            await _asyncStack.CompleteAddingAsync();
+            while (true)
+            {
+                var taleResult = await _asyncStack.TryTakeAsync();
+                if (!taleResult.Success)
+                {
+                    break;
+                }
+                Trace.WriteLine(taleResult.Item);
+            }
         }
     }
 
