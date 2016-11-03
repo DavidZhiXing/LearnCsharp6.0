a7925b8d4eef556d66b6399df2f73f32f2b270b3
DavidZhiXing <wow_only@163.com>
2016-11-03 11:00:34 +0800

    List<> Extentions

--- CsharpCookBook/CsharpCookBook/MyExtentions.cs
+++ CsharpCookBook/CsharpCookBook/MyExtentions.cs
@@ -17,5 +17,37 @@ namespace CsharpCookBook
                 Console.WriteLine($"\t({item.Ticker}) {s} {item.GainLoss}%");
             }
         }
+
+        public static IEnumerable<T> GetAll<T>(this List<T> mylist, T searchValue) =>
+            mylist.Where(t => t.Equals(searchValue));
+
+        public static T[] BinarySearchGetAll<T>(this List<T> mylist,T searchValue)
+        {
+            List<T> ret = new List<T>();
+
+            int center = mylist.BinarySearch(searchValue);
+            if (center > 0)
+            {
+                ret.Add(mylist[center]);
+                int left = center;
+                while(left > 0&&mylist[left-1].Equals(searchValue))
+                {
+                    left -= 1;
+                    ret.Add(mylist[left]);
+                }
+                var right = center;
+                while (right < (mylist.Count-1) && mylist[right + 1].Equals(searchValue))
+                {
+                    left += 1;
+                    ret.Add(mylist[right]);
+                }
+            }
+            return ret.ToArray();
+        }
+
+        public static int CountAl<T>(this List<T> mylist,T searchValue) => mylist.GetAll(searchValue).Count();
+
+        public static int BinarySearchCountAll<T>(this List<T> mylist, T searchValue) =>
+            mylist.BinarySearchGetAll(searchValue).Count();
     }
 }
